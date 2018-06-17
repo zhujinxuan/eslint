96ae0dd4daba3818f2c9abec6fae7105f17d512f
zhujinxuan <zhujinxuan@gmail.com>
2018-06-17 17:14:27 -0400

    Adds more tests

--- tests/lib/rules/padded-multilines.js
+++ tests/lib/rules/padded-multilines.js
@@ -24,31 +24,17 @@ const ruleTester = new RuleTester({ parserOptions });
 
 ruleTester.run("padded-multilines", rule, {
     valid: [
-        `function test(a) {
-  let b = a
-
-  if (a.find(x => {
-    if (x.checked) return true;
-    const {actual, expected} =x;
-    return actual === expected})) {
-    b = []
-    }
-
-  return b
-}`
+        "function test(a){\n let b = a \n\n if (a.find(x => {\n if (x.checked) return true;\n return x.actual === x.expected})) {\n b = []\n }\n\n return b\n}",
+        "function test(a){\n if (a ===1) {\n b =1\n } else if (a === 2) {\n b= 2\n } \n}",
+        "function test(a){\n let b = 0;\n\n for (;a > 0; a-- ) {\n b++\n } \n \n return b\n}"
     ],
     invalid: [
         {
-            code: `function test(a) {
-  let b = a
-  if (a.find(x => {
-    if (x.checked) return true;
-    const {actual, expected} =x;
-    return actual === expected})) {
-    b = []
-    }
-  return b
-}`,
+            code: "function test(a) { \nlet b = a\n if (a.find(x => {\n if (x.checked) return true;\n return x.actual === x.expected })) {\n b = []\n }\n return b\n}",
+            errors: [{ messageId: "before" }, { messageId: "after" }]
+        },
+        {
+            code: "function test(a) { \n let b = 0\n for (;a > 0; a-- ) {\n b++\n }\n return b\n }",
             errors: [{ messageId: "before" }, { messageId: "after" }]
         }
     ]
