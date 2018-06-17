05123331cf797ea64c70452f76a35b743254d585
zhujinxuan <zhujinxuan@gmail.com>
2018-06-17 19:07:21 -0400

    Adding more tests

--- tests/lib/rules/padded-multilines.js
+++ tests/lib/rules/padded-multilines.js
@@ -26,7 +26,9 @@ ruleTester.run("padded-multilines", rule, {
     valid: [
         "function test(a){\n let b = a \n\n if (a.find(x => {\n if (x.checked) return true;\n return x.actual === x.expected})) {\n b = []\n }\n\n return b\n}",
         "function test(a){\n if (a ===1) {\n b =1\n } else if (a === 2) {\n b= 2\n } \n}",
-        "function test(a){\n let b = 0;\n\n for (;a > 0; a-- ) {\n b++\n } \n \n return b\n}"
+        "function test(a){\n let b = 0;\n\n for (;a > 0; a-- ) {\n b++\n } \n \n return b\n}",
+        "function test(a){\n let b = 0;\n\n while (a > 0) {\n b++\n } \n \n return b\n}"
+        "function test(a){\n let b = 0;\n\n do {\n b++\n } while(a > 0) \n  \n return b\n}"
     ],
     invalid: [
         {
@@ -35,6 +37,10 @@ ruleTester.run("padded-multilines", rule, {
         },
         {
             code: "function test(a) { \n let b = 0\n for (;a > 0; a-- ) {\n b++\n }\n return b\n }",
+            errors: [{ messageId: "before" }, { messageId: "after" }]
+        },
+        {
+            code: "function test(a){\n let b = 0;\n\n do {\n b++\n } while (a > 0) \n return b\n}",
             errors: [{ messageId: "before" }, { messageId: "after" }]
         }
     ]
