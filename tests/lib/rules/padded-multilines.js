beeb58ddacb118f5b6704b464268b1a09ee347e7
zhujinxuan <zhujinxuan@gmail.com>
2018-06-17 20:38:14 -0400

    Check for if statement

--- tests/lib/rules/padded-multilines.js
+++ tests/lib/rules/padded-multilines.js
@@ -27,13 +27,15 @@ const valid = {
     "for": "function test(a){\n let b = 0;\n \nfor (;a > 0; a-- ) {\n b++\n }\n\n return b\n}",
     "while": "function test(a){\n let b = 0;\n \nwhile (a > 0) {\n b++\n }\n\n return b\n}",
     "dowhile": "function test(a){\n let b = 0;\n \ndo {\n b++\n } while (a > 0);\n \n return b\n}",
+    "call": "function test(a){\n let b = a;\n \nsetTimeout(() => {\n console.log(a)}, 1000)\n\n return b\n}"
 }
 
 const invalid = {
     "if" : "function test(a){\n let b = a\n if (a.find(x => {\n if (x.checked) return true;\n return x.actual === x.expected})) {\n b = []\n }\n return b\n}",
     "for": "function test(a){\n let b = 0;\n for (;a > 0; a-- ) {\n b++\n }\n return b\n}",
     "while": "function test(a){\n let b = 0;\n while (a > 0) {\n b++\n }\n return b\n}",
-    "dowhile": "function test(a){\n let b = 0;\n do {\n b++\n } while (a > 0); \n return b\n}"
+    "dowhile": "function test(a){\n let b = 0;\n do {\n b++\n } while (a > 0); \n return b\n}",
+    "call": "function test(a){\n let b = a;\n setTimeout(() => {\n console.log(a)}, 1000)\n return b\n}"
 }
 const validAllowReturn = {
     "if" : "function test(a){\n let b = a\n \nif (a.find(x => {\n if (x.checked) return true;\n return x.actual === x.expected})) {\n b = []\n }\n return b\n}",
@@ -46,9 +48,11 @@ ruleTester.run("padded-multilines", rule, {
     valid: [
         valid.if,
         "function test(a){\n if (a ===1) {\n b =1\n } else if (a === 2) {\n b= 2\n } \n}",
+        "function test(a){\n if (\na.find(x => {\nreturn x && x.checked})\n || \n a.find(x => {\nreturn x && x.ischecked})) {\n b =1\n } else if (a === 2) {\n b= 2\n } \n}",
         valid.for,
         valid.while,
-        valid.dowhile
+        valid.dowhile,
+        valid.call,
     ],
     invalid: [
         {
@@ -69,8 +73,12 @@ ruleTester.run("padded-multilines", rule, {
         {
             code: invalid.dowhile,
             output: valid.dowhile,
-            options: ["comment"],
             errors: [{ messageId: "before", line: 3, column: 2 }, { messageId: "after", line: 5, column: 18 }]
+        },
+        {
+            code: invalid.call,
+            output: valid.call,
+            errors: [{ messageId: "before", line: 3, column: 2 }, { messageId: "after", line: 4, column: 24 }]
         },
         {
             code: invalid.if,
@@ -95,6 +103,12 @@ ruleTester.run("padded-multilines", rule, {
             output: valid.dowhile,
             options: ["comment"],
             errors: [{ messageId: "before", line: 3, column: 2 }, { messageId: "after", line: 5, column: 18 }]
+        },
+        {
+            code: invalid.call,
+            output: valid.call,
+            options: ["comment"],
+            errors: [{ messageId: "before", line: 3, column: 2 }, { messageId: "after", line: 4, column: 24 }]
         },
         {
             code: invalid.if,
